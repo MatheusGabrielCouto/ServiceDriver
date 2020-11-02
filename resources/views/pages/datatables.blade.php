3b8012afa9a964eab667f2461fff7f6618ec40f9
Alex <alexjferman@gmail.com>
2020-11-01 23:56:17 -0400

    libs

---
+++ resources/views/pages/datatables.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.backend')
+
+@section('css_before')
+    <!-- Page JS Plugins CSS -->
+    <link rel="stylesheet" href="{{ asset('js/plugins/datatables/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('js/plugins/datatables/buttons-bs4/buttons.bootstrap4.min.css') }}">
+@endsection
+
+@section('js_after')
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables/buttons/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables/buttons/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables/buttons/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables/buttons/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables/buttons/buttons.colVis.min.js') }}"></script>
+
+    <!-- Page JS Code -->
+    <script src="{{ asset('js/pages/tables_datatables.js') }}"></script>
+@endsection
+
+@section('content')
+    <!-- Hero -->
+    <div class="bg-body-light">
+        <div class="content content-full">
+            <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+                <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">DataTables Example</h1>
+                <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">Examples</li>
+                        <li class="breadcrumb-item active" aria-current="page">Plugin</li>
+                    </ol>
+                </nav>
+            </div>
+       </div>
+    </div>
+    <!-- END Hero -->
+
+    <!-- Page Content -->
+    <div class="content">
+        <!-- Info -->
+        <div class="block block-rounded block-bordered">
+            <div class="block-header block-header-default">
+                <h3 class="block-title">Plugin Example</h3>
+            </div>
+            <div class="block-content">
+                <p>
+                    This page showcases how easily you can add a plugin’s JS/CSS assets and init it using custom JS code.
+                </p>
+            </div>
+        </div>
+        <!-- END Info -->
+
+        <!-- Dynamic Table Full -->
+        <div class="block block-rounded block-bordered">
+            <div class="block-header block-header-default">
+                <h3 class="block-title">Dynamic Table <small>Full</small></h3>
+            </div>
+            <div class="block-content block-content-full">
+                <!-- DataTables init on table by adding .js-dataTable-full class, functionality is initialized in js/pages/tables_datatables.js -->
+                <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
+                    <thead>
+                        <tr>
+                            <th class="text-center" style="width: 80px;">#</th>
+                            <th>Name</th>
+                            <th class="d-none d-sm-table-cell" style="width: 30%;">Email</th>
+                            <th style="width: 15%;">Registered</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @for ($i = 1; $i < 21; $i++)
+                        <tr>
+                            <td class="text-center"><?php echo $i; ?></td>
+                            <td class="font-w600">
+                                <a href="javascript:void(0)">John Doe</a>
+                            </td>
+                            <td class="d-none d-sm-table-cell">
+                                client{{ $i }}<em class="text-muted">@example.com</em>
+                            </td>
+                            <td>
+                                <em class="text-muted">{{ rand(2, 10) }} days ago</em>
+                            </td>
+                        </tr>
+                        @endfor
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- END Dynamic Table Full -->
+
+        <!-- Dynamic Table with Export Buttons -->
+        <div class="block block-rounded block-bordered">
+            <div class="block-header block-header-default">
+                <h3 class="block-title">Dynamic Table <small>Export Buttons</small></h3>
+            </div>
+            <div class="block-content block-content-full">
+                <!-- DataTables init on table by adding .js-dataTable-buttons class, functionality is initialized in js/pages/tables_datatables.js -->
+                <table class="table table-bordered table-striped table-vcenter js-dataTable-buttons">
+                    <thead>
+                        <tr>
+                            <th class="text-center" style="width: 80px;">#</th>
+                            <th>Name</th>
+                            <th class="d-none d-sm-table-cell" style="width: 30%;">Email</th>
+                            <th style="width: 15%;">Registered</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @for ($i = 1; $i < 21; $i++)
+                        <tr>
+                            <td class="text-center">{{ $i }}</td>
+                            <td class="font-w600">
+                                <a href="javascript:void(0)">John Smith</a>
+                            </td>
+                            <td class="d-none d-sm-table-cell">
+                                client{{ $i }}<em class="text-muted">@example.com</em>
+                            </td>
+                            <td>
+                                <em class="text-muted">{{ rand(2, 10) }} days ago</em>
+                            </td>
+                        </tr>
+                        @endfor
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- END Dynamic Table with Export Buttons -->
+    </div>
+    <!-- END Page Content -->
+@endsection
